9eba30763a444f7343446a1d16988badb3b089c3
Miguel García Reimers <mgarciareimers@gmail.com>
2020-05-23 15:05:21 +0200

    Download of all hacker news data.

--- webScraping/venv/main.py
+++ webScraping/venv/main.py
@@ -1,7 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint  # To format printing
+import sys
 
 
+# Method that sorts the news by votes (DESC).
+def sort_stories_by_votes(news_list):
+    return sorted(news_list, key=lambda k: k['votes'], reverse=True)
+
+
+# Method that creates the custom hacker news list.
 def create_custom_hacker_news(links, subtexts):
     hacker_news = []
 
@@ -12,20 +20,45 @@ def create_custom_hacker_news(links, subtexts):
         vote = subtexts[index].select('.score')
 
         if len(vote):
-            hacker_news.append({'title': title, 'href': href, 'votes': int(vote[0].getText().replace(' points', ''))})
+            votes = int(vote[0].getText().replace(' points', ''))
+
+            if votes >= 100:
+                hacker_news.append({'title': title, 'href': href, 'votes': votes})
 
     return hacker_news
 
 
-url = 'https://news.ycombinator.com/news'
-response = requests.get(url)
+print('* Welcome to Hacker News Top News')
 
-# Parse the response to HTML
-soup = BeautifulSoup(response.text, 'html.parser')
+download = True
+page_number = 0
+custom_hacker_news = []
 
-# Get the links and the votes.
-links = soup.select('.storylink')
-subtexts = soup.select('.subtext')
+print('  Starting download...')
+
+while download:
+    page_number = page_number + 1
+    url = f'https://news.ycombinator.com/news?p={ page_number }'
+    response = requests.get(url)
+
+    # Parse the response to HTML
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Get the links and the votes.
+    links = soup.select('.storylink')
+    subtexts = soup.select('.subtext')
+
+    if len(links) <= 0 or len(subtexts) <= 0:
+        download = False
+        continue
+
+    for new in create_custom_hacker_news(links, subtexts):
+        custom_hacker_news.append(new)
+
+    sys.stdout.write(f'\r  Current Page Number: { page_number }')
+    sys.stdout.flush()
 
 # Get the custom hacker news.
-print(create_custom_hacker_news(links, subtexts))
+custom_hacker_news = sort_stories_by_votes(custom_hacker_news)
+print('\n\n Custom news: \n')
+pprint.pp(f'{custom_hacker_news}')
