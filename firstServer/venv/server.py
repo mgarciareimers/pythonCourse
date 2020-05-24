314471ce1b85bfc91ea8e7eaa532a86ef2003cf8
Miguel García Reimers <mgarciareimers@gmail.com>
2020-05-24 11:46:44 +0200

    Templates rendered with flask render_template.

--- firstServer/venv/server.py
+++ firstServer/venv/server.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 # To use start the server (in terminal):
 # export FLASK_APP=server.py
+# To change to DEBUG mode, run "export FLASK_ENV=development".
 # flask run
-# To change to DEBUG mode, run "FLASK_ENV=development" and then "flask run".
 
 # Root route.
 @app.route('/')
 def hello_world():
-    return 'Hello, World! :)'
+    return render_template('./index.html')  # Do not include the parent folder ('template'). Views need to be in templates folder.
+
+
+# Root route.
+@app.route('/about')
+def about():
+    return render_template('./about.html')
