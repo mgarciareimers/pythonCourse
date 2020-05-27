9dbae1256a08ceab7977920f2be21db6ede9bfff
Miguel García Reimers <mgarciareimers@gmail.com>
2020-05-27 21:49:24 +0200

    Front developed + Start of database lectures: data can be stored in .csv and .txt.

--- portfolio/venv/server.py
+++ portfolio/venv/server.py
@@ -1,5 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import csv
 app = Flask(__name__)
+
+
+def write_to_txt_file(data):
+    with open('database/data.txt', mode='a') as database:
+        file = database.write(f'{ data["email"] },{ data["subject"] },{ data["message"] }\n')
+
+
+def write_to_csv_file(data):
+    with open('database/data.csv', newline='', mode='a') as database:
+        csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([data["email"], data["subject"], data["message"]])
 
 
 @app.route('/')
@@ -10,3 +22,16 @@ def index():
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+
+        # write_to_txt_file(data)
+        write_to_csv_file(data)
+
+        return render_template('thankyou.html', data=data)
+    else:
+        return 'Something went wrong!'
